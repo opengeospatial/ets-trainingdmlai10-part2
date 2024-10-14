860f07d50c9ad4a94acd5284c1f36f80452e1af5
jiann <jiannkoh@gis.tw>
2024-10-14 15:56:59 +0800

    implement abstract test 10

--- src/main/java/org/opengis/cite/trainingdmlai10part2/base/GeospatialTypeTest.java
+++ src/main/java/org/opengis/cite/trainingdmlai10part2/base/GeospatialTypeTest.java
@@ -1,13 +1,21 @@
 package org.opengis.cite.trainingdmlai10part2.base;
 
-import org.apache.jena.atlas.lib.NotImplemented;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.ValidationMessage;
+import org.opengis.cite.trainingdmlai10part2.BaseJsonSchemaValidatorTest;
 import org.opengis.cite.trainingdmlai10part2.CommonFixture;
 import org.opengis.cite.trainingdmlai10part2.SuiteAttribute;
+import org.opengis.cite.trainingdmlai10part2.util.JsonUtils;
+import org.testng.Assert;
 import org.testng.ITestContext;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 public class GeospatialTypeTest extends CommonFixture {
     private File testSubject;
@@ -38,6 +46,40 @@ public class GeospatialTypeTest extends CommonFixture {
 
     @Test(description = "Implements Abstract Test 10 (/conf/base/geospatialtype/feature)")
     public void verifyFeature() {
-        throw new NotImplemented();
+        if (!testSubject.isFile()) {
+            Assert.assertTrue(testSubject.isFile(), "No file selected. ");
+        }
+
+        String schemaToApply = SCHEMA_PATH + "geojson/" + "Feature.json";
+        StringBuffer sb = new StringBuffer();
+
+        try {
+            BaseJsonSchemaValidatorTest tester = new BaseJsonSchemaValidatorTest();
+
+            JsonSchema schema = tester.getSchema(schemaToApply);
+            JsonNode rootNode = tester.getNodeFromFile(testSubject);
+
+            String[] arrayToFetch = {"object"};
+
+            List<JsonNode> nodes = JsonUtils.findNodesByNames(rootNode, arrayToFetch);
+
+            for (JsonNode targetNode : nodes) {
+                String nodeClass = targetNode.getClass().toString();
+                if (nodeClass.endsWith("com.fasterxml.jackson.databind.node.ObjectNode")) {
+                    Set<ValidationMessage> errors = schema.validate(targetNode);
+                    Iterator it = errors.iterator();
+                    while (it.hasNext()) {
+                        sb.append("Item " + targetNode + " has error " + it.next() + ".\n");
+                    }
+                } else {
+                    sb.append("Item " + targetNode + " is not an object.\n");
+                }
+            }
+
+        } catch (Exception e) {
+            sb.append(e.getMessage());
+            e.printStackTrace();
+        }
+        Assert.assertTrue(sb.toString().length() == 0, sb.toString());
     }
 }
